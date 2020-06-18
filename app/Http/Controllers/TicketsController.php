b1663343b8cb697cda4b70f3e1315c48fc796ddf
Shovon Das <shovondas26@gmail.com>
2020-06-18 17:24:17 +0600

    updated

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -13,7 +13,7 @@ class TicketsController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.ticket.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class TicketsController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.ticket.create');
     }
 
     /**
