52f7e07b596b2b25e208ca635d2021f6aa944320
Shovon Das <shovondas26@gmail.com>
2020-06-25 12:52:47 +0600

    extra agent in seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -16,7 +16,7 @@ class UserTableSeeder extends Seeder
                 'first_name'    => 'Admin',
                 'last_name'     => 'AndIt',
                 'phone'         => '01546515444',
-                'agency'         => 'abc',
+                'agency'         => '',
                 'email'         => 'system@admin.com',
                 'password'      => '123456', //123456
                 'created_at'    => now(),
@@ -26,12 +26,28 @@ class UserTableSeeder extends Seeder
         $role = \Sentinel::findRoleBySlug('admin');
         $role->users()->attach($user->id);
         $users = [
-            'name'          => 'Agent 1',
+            'name'          => 'AndiT',
             'first_name'    => 'Shariful',
             'last_name'     => 'Islam',
             'phone'         => '01546515444',
-            'agency'         => 'abc',
-            'email'         => 'system@agent.com',
+            'agency'         => '1',
+            'email'         => 'shoriful@agent.com',
+            'password'      => '123456', //123456
+            'created_at'    => now(),
+            'updated_at'    => now()
+        ];
+        
+        $user = \Sentinel::registerAndActivate($users);
+        $role = \Sentinel::findRoleBySlug('agent');
+        $role->users()->attach($user->id);
+
+        $users = [
+            'name'          => 'Impex',
+            'first_name'    => 'Mehedi',
+            'last_name'     => 'Khan',
+            'phone'         => '01546515444',
+            'agency'         => '2',
+            'email'         => 'mehedi@agent.com',
             'password'      => '123456', //123456
             'created_at'    => now(),
             'updated_at'    => now()
