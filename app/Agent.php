e3a095ce13a3ba0b8b50ac77a30e3c5758b6845d
Shovon Das <shovondas26@gmail.com>
2020-06-25 12:31:04 +0600

    relation added

--- app/Agent.php
+++ app/Agent.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Agent extends Model
 {
     protected $fillable = ['name', 'email', 'password', 'username', 'phone', 'permissions', 'mobile', 'type'];
+
+    public function tickets(){
+        return $this->hasMany(Agents::class);
+    }
 }
