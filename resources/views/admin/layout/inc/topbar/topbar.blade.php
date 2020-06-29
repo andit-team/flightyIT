f7868505667c69305823f32b16a3790bf7bb6790
Shovon Das <shovondas26@gmail.com>
2020-06-29 14:38:47 +0600

    new top bar

--- resources/views/admin/layout/inc/topbar/topbar.blade.php
+++ resources/views/admin/layout/inc/topbar/topbar.blade.php
@@ -5,20 +5,12 @@
         <!-- ============================================================== -->
         <div class="navbar-header">
             <a class="navbar-brand" href="{{ url('/', []) }}">
-                <!-- Logo icon --><b>
-                    <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
-                    <!-- Dark Logo icon -->
-                    {{-- <img src="{{ !empty(session()->get('settings')[0]['logo'])?asset(Storage::url(session()->get('settings')[0]['logo'])) : url('logo.png') }}" alt="homepage" class="dark-logo"> --}}
+                <b>
                     <!-- Light Logo icon -->
                     <img src="{{asset('logo-sort.png')}}" alt="">
-                    {{-- <img src="{{!empty(session()->get('settings')[0]['logo']) ? asset(Storage::url(session()->get('settings')[0]['logo'])) :  asset('logo.png')}}"  height="34" width="33" alt="user"> --}}
                 </b>
                 <!--End Logo icon -->
-                <!-- Logo text --><span>
-                 <!-- dark Logo text -->
-                 {{-- <img src="{{ asset('material') }}/assets/images/logo-text.png" alt="homepage" class="dark-logo" /> --}}
-                 <!-- Light Logo text -->
-                 {{-- <img src="{{ asset('material') }}/assets/images/logo-light-text.png" class="light-logo" alt="homepage" /> --}}
+                <span>
                  <b class="light-logo">{{session()->get('settings')[0]['site_name']}}</b>
                 </span> </a>
         </div>
@@ -30,96 +22,31 @@
             <!-- toggle and nav items -->
             <!-- ============================================================== -->
             <ul class="navbar-nav mr-auto mt-md-0">
-                <!-- This is  -->
-                <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="mdi mdi-menu"></i></a> </li>
-                <li class="nav-item"> <a class="nav-link sidebartoggler hidden-sm-down text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
-              
             </ul>
             <!-- ============================================================== -->
             
                 
             <!-- ============================================================== -->
             <ul class="navbar-nav my-lg-0">
-                <!-- ============================================================== -->
-                <!-- Comment -->
-                <!-- ============================================================== -->
-                
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-muted text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-message"></i>
-                        @if(App\Notification::where('user',Sentinel::getUser()->id)->where('is_read',0)->count() > 0)
-                            <div class="notify"> <span class="heartbit"></span> <span class="point"></span> </div>
-                        @endif
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right mailbox scale-up">
-                        <ul>
-                            <li>
-                                <div class="drop-title">Notifications</div>
-                            </li>
-                            <?php $notifactions = App\Notification::where('user',Sentinel::getUser()->id)->where('is_read',0)->get() ;?>
-                            <li>
-                                <div class="message-center">
-                                    @forelse($notifactions as $data)
-                                        <a href="{{url('system-admin/users/notification')}}" class="updateNotification" data-id={{$data->id}}>
-                                            <div class="btn btn-success btn-circle"><i class="fa fa-bell"></i></div>
-                                            <div class="mail-contnet">
-                                            <h5>{{Sentinel::findById($data->from)->name}}</h5> <span class="mail-desc">{{$data->content}}</span>
-                                            </div>
-                                        </a>
-                                    @empty
-                                    @endforelse
-                                </div>
-                            <li>
-                                <a class="nav-link text-center" href="{{url('system-admin/users/notification')}}"> <strong>Check all notifications</strong> <i class="fa fa-angle-right"></i> </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <!-- ============================================================== -->
-                <!-- End Comment -->
-                <!-- ============================================================== -->
-                <!-- ============================================================== -->
-                <!-- Messages -->
-                <!-- ============================================================== -->
-                
-                @if(Core::isAdmin())
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" id="2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-email"></i>
-                        <?php $emails = DB::table('mailboxes')->where('status','unread')->orderBy('id','desc');
-                            $count = $emails->count();
-                            $emails = $emails->get();
-                        ?>
-                        @if($count > 0)
-                            <div class="notify"> <span class="heartbit"></span> <span class="point"></span> </div>
-                        @endif
-                    </a>
-                    <div class="dropdown-menu mailbox dropdown-menu-right scale-up" aria-labelledby="2">
-                        <ul>
-                            <li>
-                                <div class="drop-title">Emails</div>
-                            </li>
-                            <li>
-                                <div class="message-center">
-                                    <!-- Message -->
-                                    @forelse($emails as $data)
-                                    <a href="{{url('mailbox/detail/'.$data->id)}}">
-                                        <div class="btn btn-success btn-circle"><i class="mdi mdi-email"></i></div>
-                                        <div class="mail-contnet">
-                                        <h5>{{$data->to}}</h5> <span class="mail-desc">{{Core::limit_text(strip_tags($data->message),5)}}</span> <span class="time">9:30 AM</span> </div>
-                                    </a>
-                                    @endforeach
-                                </div>
-                            </li>
-                            <li>
-                                <a class="nav-link text-center" href="{{url('mailbox')}}"> <strong>See all e-Mails</strong> <i class="fa fa-angle-right"></i> </a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
+
+                {{-- agent --}}
+                @if(Sentinel::hasAccess('agency'))
+                    <li class="nav-item"><a href="{{url('system-admin/agency/create')}}" class="nav-link waves-effect waves-dark"><i class="mdi mdi-settings-box"></i> Add Agency</a></li>
                 @endif
-                <!-- ============================================================== -->
-                <!-- End Messages -->
-                <!-- ============================================================== -->
-                <!-- ============================================================== -->
+                {{-- agent --}}
+                @if(Sentinel::hasAccess('agent'))
+                    <li class="nav-item"><a href="{{url('system-admin/agent/create')}}" class="nav-link waves-effect waves-dark"><i class="mdi mdi-settings-box"></i> Add Agent</a></li>
+                @endif
+                {{-- Add Ticket--}}
+                @if(Sentinel::hasAccess('tickets'))
+                    <li class="nav-item"><a href="{{url('system-admin/ticket/create')}}" class="nav-link waves-effect waves-dark {{ request()->is('ticket/system-setting/create') ? 'active' : '' }}"><i class="mdi mdi-settings-box"></i> Add Ticket</a></li>
+                @endif
+
+                {{-- Tickets Search--}}
+                @if(Sentinel::hasAccess('tickets'))
+                    <li class="nav-item"><a href="{{url('system-admin/ticket/')}}" class="nav-link waves-effect waves-dark {{ request()->is('ticket/system-setting/') ? 'active' : '' }}"><i class="mdi mdi-settings-box"></i> Search Ticket</a></li>
+                @endif
+
                 <!-- Profile -->
                 <!-- ============================================================== -->
                 <?php $userImage = Core::getinfo('users','id',Sentinel::getUser()->id)?>
@@ -137,40 +64,11 @@
                             </li>
                             <li role="separator" class="divider"></li>
                             <li><a href="{{url('system-admin/myprofile')}}"><i class="ti-user"></i> My Profile</a></li>
-                            <li><a href="{{url('system-admin/users/activities')}}"><i class="mdi mdi-run-fast"></i> My Activities</a></li>
-                            <li><a href="{{url('system-admin/users/notification')}}"><i class="mdi mdi-alarm-multiple"></i> My Notification</a></li>
                             <li role="separator" class="divider"></li>
                             <li><a href="{{url('system-admin/myprofile')}}"><i class="ti-settings"></i> Account Setting</a></li>
                             <li role="separator" class="divider"></li>
                             <li><a href="{{url('system-admin/logout')}}"><i class="fa fa-power-off"></i> Logout</a></li>
                         </ul>
-                    </div>
-                </li>
-                <!-- ============================================================== -->
-                <!-- Language -->
-                <!-- ============================================================== -->
-                @php
-                    if(\Session::has('locale')){
-                        $language = Session::get('locale');
-                        if($language == 'bn'){
-                            $flag = 'bd';
-                        }
-                        if($language == 'hi'){
-                            $flag = 'in';
-                        }
-                        if($language == 'en'){
-                            $flag = 'us';
-                        }
-                    }else{
-                        $flag = 'us';
-                    }
-                @endphp
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="flag-icon flag-icon-{{$flag}}"></i></a>
-                    <div class="dropdown-menu dropdown-menu-right scale-up"> 
-                        <a class="dropdown-item" href="{{url('locale/bn')}}"><i class="flag-icon flag-icon-bd"></i> Bangla</a> 
-                        <a class="dropdown-item" href="{{url('locale/hi')}}"><i class="flag-icon flag-icon-in"></i> Hindi</a>
-                        <a class="dropdown-item" href="{{url('locale/en')}}"><i class="flag-icon flag-icon-us"></i> English</a>
                     </div>
                 </li>
             </ul>
